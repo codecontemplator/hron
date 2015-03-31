cdc85ac0884ec6abcf3afefbe3e014e65ed32822
Daniel Brännström <danielbrannstrom@hotmail.com>
2015-03-31 16:31:46 +0200

    handles comments

--- languages/haskell/namedlist.hs
+++ languages/haskell/namedlist.hs
@@ -13,6 +13,7 @@ input_text = unlines [
     "listName:",
     "  item1",
     "  item2",
+    "#comment",
     "  item3"
   ]
 
@@ -28,11 +29,28 @@ data NamedList = NamedList Name [Item]
 type Name = String
 type Item = String
 
+withBlock2 f a p = withPos $ do
+    r1 <- a
+    r2 <- block2 p
+    return (f r1 r2)
+
+block2 p = withPos $ do
+    r <- many1 ((checkIndent >> p) <|> aComment)
+    return r
+
 aNamedList :: IParser NamedList
 aNamedList = do
-  b <- withBlock NamedList aName anItem
+  b <- withBlock2 NamedList aName anItem
   spaces
   return b
+
+aComment :: IParser String
+aComment = do
+	spaces
+	char '#'
+	x <- many1 alphaNum
+	spaces
+	return x
 
 aName :: IParser Name
 aName = do
