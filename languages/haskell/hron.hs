55397f1e89b1656a4c15bd2ece5c977e94718c3a
Daniel Brännström <danielbrannstrom@hotmail.com>
2015-03-31 21:17:18 +0200

    Implemented show for action logs

--- languages/haskell/hron.hs
+++ languages/haskell/hron.hs
@@ -1,25 +1,46 @@
 module HRON where
 
 import Text.Parsec(modifyState)
---import Text.Parsec.Pos
---import Text.Parsec.Indent
 import Text.ParserCombinators.Parsec
-import Control.Monad (replicateM)
-import Debug.Trace
 
 type IndentParser a = GenParser Char Int a
 
-data Preprocessor = Preprocessor String deriving(Show)
-data ValueLine = NonEmptyLine String | CommentLine String | EmptyLine  deriving(Show)
-data Member = Value String [ValueLine] | Object String [Member] | Comment String | Empty deriving(Show)
-data SyntaxTree = SyntaxTree [Preprocessor] [Member] deriving(Show)
+data Preprocessor = Preprocessor String
+data ValueLine = ContentLine String | CommentLine String | EmptyLine
+data Member = Value String [ValueLine] | Object String [Member] | Comment String | Empty
+data HRON = HRON [Preprocessor] [Member]
 
-traceM :: Monad m => String -> m ()
-traceM msg = trace msg (return ())
+instance Show Preprocessor where
+	show (Preprocessor s) = "Preprocessor:" ++ s
+
+instance Show ValueLine where
+	show (ContentLine s) = "ContentLine:" ++ s
+	show (CommentLine s) = "CommentLine:" ++ s 
+	show EmptyLine       = "EmptyLine:"
+
+instance Show Member where
+	show (Value tag value_lines) = 
+		"Value_Begin:" ++ tag ++ "\n" ++
+		unlines (map show value_lines) ++
+		"Value_End:" ++ tag
+	show (Object tag members) = 
+		"Object_Begin" ++ tag ++ "\n" ++
+		unlines (map show members) ++
+		"Object_End:" ++ tag
+	show (Comment s) = "Comment:" ++ s
+	show Empty = "Empty"
+
+instance Show HRON where
+	show (HRON preprocessors members) =
+		unlines (map show preprocessors) ++
+		unlines (map show members)
+
+--traceM :: Monad m => String -> m ()
+--traceM msg = trace msg (return ())
 
 void p = p >> (return ())
 
-eol = void(oneOf "\n") --	<|> eof
+eol = void(oneOf "\n") -- <|> eof
 any_indention = void(manyTill space eol)  -- fix this
 
 indent = do
@@ -45,6 +66,13 @@ comment_string = do
 	s <- hron_string
 	return s
 
+preprocessor = do
+	char '!'
+	s <- hron_string
+	return $ Preprocessor s
+
+preprocessors = many preprocessor
+
 empty_line = do
 	empty_string 
 --	eol
@@ -59,7 +87,7 @@ nonempty_line = do
 	indention 
 	s <- hron_string
 --	eol
-	return $ NonEmptyLine s
+	return $ ContentLine s
 
 value_line = nonempty_line <|> comment_line <|> empty_line 
 
@@ -69,7 +97,6 @@ value = do
 	indention
 	char '='
 	tag <- hron_string
-	traceM $ "tag: " ++ show tag
 	--eol
 	indent
 	lines <- value_lines
@@ -99,9 +126,15 @@ object = do
 member =  value <|> object <|> comment <|> empty	
 members = many member
 
-hron_parse input = runParser members 0 "" input
+hron = do
+	pp <- preprocessors
+	mbrs <- members
+	return $ HRON pp mbrs
+
+hron_parse input = runParser hron 0 "" input
 
 input_text = unlines [
+	"!abc",
     "=Welcome message",
     "\tHello there",
     "\tThis is hron speaking",
