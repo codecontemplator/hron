a87636514ec59af753b418364ee0d745826f1e4e
Daniel Brännström <danielbrannstrom@hotmail.com>
2015-03-31 23:08:01 +0200

    Hello world parsed successfully

--- languages/haskell/hron.hs
+++ languages/haskell/hron.hs
@@ -2,6 +2,7 @@ module HRON where
 
 import Text.Parsec(modifyState)
 import Text.ParserCombinators.Parsec
+import Debug.Trace
 
 type IndentParser a = GenParser Char Int a
 
@@ -24,19 +25,19 @@ instance Show Member where
 		unlines (map show value_lines) ++
 		"Value_End:" ++ tag
 	show (Object tag members) = 
-		"Object_Begin" ++ tag ++ "\n" ++
+		"Object_Begin:" ++ tag ++ "\n" ++
 		unlines (map show members) ++
 		"Object_End:" ++ tag
-	show (Comment s) = "Comment:" ++ s
-	show Empty = "Empty"
+	show (Comment s) = "Comment:0," ++ s  -- todo: must count number of spaces
+ 	show Empty = "Empty"
 
 instance Show HRON where
 	show (HRON preprocessors members) =
 		unlines (map show preprocessors) ++
 		unlines (map show members)
 
---traceM :: Monad m => String -> m ()
---traceM msg = trace msg (return ())
+traceM :: Monad m => String -> m ()
+traceM msg = trace msg (return ())
 
 void p = p >> (return ())
 
@@ -44,15 +45,18 @@ eol = void(oneOf "\n") -- <|> eof
 any_indention = void(manyTill space eol)  -- fix this
 
 indent = do
+	traceM $ "++indent"
 	modifyState (\i -> i + 1)
 	return ()
 
 dedent = do
+	traceM $ "--indent"
 	modifyState (\i -> i - 1)
 	return ()
 
 indention = do
 	i <- getState
+	traceM $ "indent size: " ++ show i
 	count i (char '\t')
 	return ()
 
@@ -61,9 +65,12 @@ empty_string = void $ manyTill space (try eol)
 hron_string = manyTill anyChar (try eol)
 
 comment_string = do
-	any_indention
-	char '#'
+	--any_indention
+	traceM "comment_string 1"
+	try $ spaces >> char '#'
+	traceM "comment_string 2"
 	s <- hron_string
+	traceM $ "CommentString:" ++ s
 	return s
 
 preprocessor = do
@@ -91,12 +98,13 @@ nonempty_line = do
 
 value_line = nonempty_line <|> comment_line <|> empty_line 
 
-value_lines = many value_line
+value_lines = many (try value_line)
 
 value = do
-	indention
-	char '='
+	traceM $ "+value+"
+	try $ indention >> char '='	
 	tag <- hron_string
+	traceM $ "value: " ++ tag
 	--eol
 	indent
 	lines <- value_lines
@@ -104,19 +112,24 @@ value = do
 	return $ Value tag lines
 
 empty = do
+	traceM $ "+empty+"
 	empty_string
 --	eol
+	traceM $ "empty:"
 	return Empty
 
 comment = do 
+	traceM $ "+comment+"
 	cs <- comment_string 
 --	eol
+	traceM $ "comment: " ++ cs
 	return $ Comment cs
 
 object = do
-	indention
-	char '@'
+	traceM $ "+object+"
+	try $ indention >> char '@'
 	tag <- hron_string
+	traceM $ "Object: " ++ tag
 --	eol
 	indent
 	mbs <- members
@@ -145,3 +158,54 @@ input_text = unlines [
     "\t=V1",
     "\t\t999"
   ]
+
+input_text2 = unlines [
+	"# object values are started with @",
+	"@Common",
+	"\t=LogPath",
+	"\t\tLogsCurrentDay",
+	"\t=WelcomeMessage",
+	"\t\tHello there!",
+	"",
+	"\t\tString values in hron is started with"
+	]
+
+input_text3 = unlines [
+	"# object values are started with '@'",
+	"@Common",
+	"\t=LogPath",
+	"\t\tLogs\\CurrentDay",
+	"\t=WelcomeMessage",
+	"\t\tHello there!",
+	"",
+	"\t\tString values in hron is started with '='",
+	"",
+	"\t\tJust as with Python in hron the indention is important",
+	"",
+	"\t\tIdention promotes readability but also allows hron string values ",
+	"\t\tto be multi-line and hron has no special letters that requires escaping",
+	"\t\t",
+	"\t\tLetters like this causes hron no problems: &<>\\'@=",
+	"",
+	"\t\tThis helps readability",
+	"",
+	"",
+	"@DataBaseConnection",
+	"\t=Name",
+	"\t\tCustomerDB",
+	"\t=ConnectionString",
+	"\t\tData Source=.\\SQLEXPRESS;Initial Catalog=Customers",
+	"\t=TimeOut",
+	"\t\t10",
+	"\t@User",
+	"\t\t=UserName",
+	"\t\t\tATestUser",
+	"\t\t=Password",
+	"\t\t\t123",
+	"@DataBaseConnection",
+	"\t=Name",
+	"\t\tPartnerDB",
+	"\t=ConnectionString",
+	"\t\tData Source=.\\SQLEXPRESS;Initial Catalog=Partners\t",
+	""
+	]
--- languages/haskell/test.hs
+++ languages/haskell/test.hs
@@ -1,6 +1,33 @@
 import Test.HUnit
 import HRON
+import System.IO  
+import Control.Monad
 
-test1 = TestCase (assertEqual "my assert" "123" "123")
+fromRight           :: Either a b -> b
+fromRight (Left _)  = error "Either.Unwrap.fromRight: Argument takes form 'Left _'" -- yuck
+fromRight (Right x) = x
 
-tests = TestList [TestLabel "test1" test1]
+testHelloWorld = TestCase $
+	do
+		hronFile <- openFile "../../reference-data/helloworld.hron" ReadMode
+		hSetEncoding hronFile utf8_bom
+		hron <- hGetContents hronFile
+		putStrLn hron
+		hronActionLogFile <- openFile "../../reference-data/helloworld.hron.actionlog" ReadMode
+		hSetEncoding hronActionLogFile utf8_bom
+		hronActionLog <- hGetContents hronActionLogFile
+		let tree = hron_parse (hron ++ "\n") -- todo: handle missing newline at end of file properly in parser
+		case tree of 
+		  Left x -> putStrLn $ "failed " ++ show x
+		  Right _ -> putStrLn "ok" 
+		let log = show $ fromRight tree 
+		putStrLn "------------------------"
+		putStrLn log
+		putStrLn "------------------------"
+		assertEqual "action logs should be equal" hronActionLog log
+
+tests = TestList [
+	TestLabel "hello world" testHelloWorld
+	]
+
+rt = runTestTT tests
